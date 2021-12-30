1961a635129d7d5bcb2556358c61d7bdf577c1b8
pabloaturnes <pabloaturnes@gmail.com>
2021-12-30 10:46:58 -0300

    menu y boton funcionando

--- menu.js
+++ menu.js
@@ -2,12 +2,18 @@
 export default function menu(){
 
 let $menuButton = document.querySelector(".menu-button");
-let $menu = document.querySelector("nav ul")
+let $menu = document.querySelector("nav ul");
+let $linkMenu = document.querySelectorAll("nav ul li a");
 
 $menuButton.addEventListener("click", ()=>{
-
     $menu.classList.toggle("menu-active");
+})
 
+$linkMenu.forEach(link => {
+
+    link.addEventListener("click", ()=>{
+        $menu.classList.remove("menu-active");
+    })
 
 })
 
